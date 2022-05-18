aa980dd4d842256ef433f7e95cbe51bd2a420637
sachinthaakesh <akeshramasinghe@gmail.com>
2022-05-18 13:10:25 +0530

    Update weather-service.service.ts
    Develop getWeatherData method

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,12 @@ import { Injectable } from '@angular/core';
 export class WeatherServiceService {
 
   constructor( private http: HttpClient) {}
+
+  getweatherData(cityName: string){
+    this.http.get(environment.weatherBaseUrl,{
+      headers:new HttpHeaders().set(environment.XRapidAPIHostname,environment.XRapidAPIHostvalue)
+      .set(environment.XRapidAPIKeyname,environment.XRapidAPIKeyvalue),
+      params:new HttpParams().set('q',cityName).set('units','imperial').set('mode','json')
+    });
+  }
 }
